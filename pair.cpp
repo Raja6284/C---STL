8223025e9749d3355e86a9e2363622ba07e9160c
Raja Kumar <rajaraja360parth@gmail.com>
2023-12-17 20:17:59 +0530

    basic operation of pair

---
+++ pair.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    pair<int, int> p;
+
+    p.first = 5;
+    p.second = 10;
+
+    cout << "{" << p.first << "," << p.second << "}"<<endl;
+
+    // chaining of pair
+
+    pair<int, int> p1;
+
+    p1.first = 3;
+    //p1.second.first = 6;
+    // p1.second.second = 9;
+
+    vector<pair<int, int>> v;       //constructing vector of pair
+
+    int n;
+    cout << "Enter the value of n:";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        v.push_back({x, y});
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].first << " " << v[i].second << endl;
+    }
+
+    return 0;
+}
